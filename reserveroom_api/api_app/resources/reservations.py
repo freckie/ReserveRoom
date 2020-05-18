75b19547f0c89b3e79f565717141ffdf0e2de761
Freckie <freckie@frec.kr>
2020-05-18 14:04:18 +0900

    Add: GET /reservations

--- reserveroom_api/api_app/resources/reservations.py
+++ reserveroom_api/api_app/resources/reservations.py
@@ -10,18 +10,35 @@ class GETReservations(Resource):
     @jwt_required
     def get(self):
         # Handle body parameters
+        params = {
+            'user_email': None,
+            'query': None
+        }
+        if 'user_email' in request.args:
+            params['user_email'] = request.args['user_email']
+        if 'query' in request.args:
+            params['query'] = request.args['query']
 
         # Querying
-        query = '''
+        _sql_email, _sql_query = '', ''
+        if params['user_email'] is not None:
+            _sql_email = ' AND R.user_email="{}" '.format(params['user_email'])
+        
+        if params['query'] is not None:
+            _sql_query = ' AND (R.subject LIKE "%{}%" OR U.name LIKE "%{}%") '.format(params['query'], params['query'])
+
+        sql = '''
             SELECT R.id "id", R.classroom_id "classroom_id", CL.id "college_id", CL.name "college_name", 
-	            R.user_email "user_email", U.name "user_name", R.start_time "start_time", R.end_Time "end_time", R.subject "subject"
+                R.user_email "user_email", U.name "user_name", R.start_time "start_time", R.end_Time "end_time", R.subject "subject"
             FROM reservations R, users U, colleges CL, classrooms CR
             WHERE R.user_email=U.email
                 AND R.classroom_id=CR.id
                 AND CR.college_id=CL.id
+                {}
+                {}
             ORDER BY id DESC;
-        '''
-        rows = app.db_driver.execute_all(query)
+        '''.format(_sql_email, _sql_query)
+        rows = app.db_driver.execute_all(sql)
 
         result = {
             'reservation_count': 0,

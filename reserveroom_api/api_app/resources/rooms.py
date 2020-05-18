88fe51013f006b225e95cae1ddcde66d2bec611e
JHyunB <whdgus4892@gmail.com>
2020-05-18 16:01:17 +0900

    Fix

--- reserveroom_api/api_app/resources/rooms.py
+++ reserveroom_api/api_app/resources/rooms.py
@@ -9,22 +9,21 @@ class GETRooms(Resource):
         # Parse body params
         try:
             parser = reqparse.RequestParser()
-            parser.add_argument('college_id', required=False, type=int)
-            parser.add_argument('capacity', required=False, type=int)
+            parser.add_argument('college_id', required=False, default=1, type=int)
+            parser.add_argument('capacity', required=False, default=0, type=int)
             args = parser.parse_args()
         except:
             pass
         _college_id = args['college_id']
         _capacity = args['capacity']
-        print(_college_id)
-        print(_capacity)
+
         # Querying
         query = '''
             SELECT C.name, R.id, R.capacity
             FROM classrooms R, colleges C
             WHERE R.college_id=C.id
         '''
-        query += 'AND R.college_id = ' + str(_college_id) + ' AND R.capacity > ' + str(_capacity * 2)
+        query += 'AND R.college_id = ' + str(_college_id) + ' AND R.capacity >= ' + str(_capacity * 2)
 
         rows = app.db_driver.execute_all(query)
 
@@ -41,7 +40,70 @@ class GETRooms(Resource):
         
         return ok_response(result)
 
-# GET /rooms/<room_id>
+# GET /rooms/detail
 class GETRoomsDetail(Resource):
-    def get(self, room_id):
-        pass
+    def get(self):
+         # Parse body params
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('room_id', required=False, type=str)
+            args = parser.parse_args()
+        except:
+            pass
+        _room_id = args['room_id']
+        # Querying
+        query = '''
+            SELECT subject, start_time, end_time
+            FROM reservations
+            WHERE classroom_id = %s
+        '''
+        
+        rows = app.db_driver.execute_all(query,(_room_id))
+
+         # Fetch
+        result = {
+            'times': []
+        }
+        for row in rows:
+            result['times'].append({
+                'subject': row['subject'],
+                'start_time': row['start_time'].strftime('%Y-%m-%d %H:%M'),
+                'end_time': row['end_time'].strftime('%Y-%m-%d %H:%M')
+            })
+        
+        return ok_response(result)
+
+# GET /rooms/available
+class GETRoomsAvailable(Resource):
+    def get(self):
+         # Parse body params
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('room_id', required=False, type=str)
+            parser.add_argument('start_time',required=True, type=str)
+            parser.add_argument('end_time',required=True, type=str)
+            args = parser.parse_args()
+        except:
+            pass
+        _room_id = args['room_id']
+        _start_time = args['start_time'].strptime('%Y-%m-%d %H:%M')
+        _end_time = args['end_time'].strptime('%Y-%m-%d %H:%M')
+        # Querying
+        query = '''
+            SELECT start_time, end_time
+            FROM reservations
+            WHERE classroom_id = %s
+        '''
+
+        rows = app.db_driver.execute_all(query,(_room_id))
+        for row in rows:
+            if ((_start_time > row['start_time']) and (_start_time < row['end_time'])) 
+            or ((_end_time < row['end_time']) and (_end_time > row['start_time']):
+                
+         # Fetch
+        result = {
+            'times': []
+        }
+        
+        
+        return ok_response(result)

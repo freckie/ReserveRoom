86118904952b6f5c4fa3e01814e2a30ce4ec794c
freckie <freckie@frec.kr>
2020-05-18 23:06:52 +0900

    Add: PUT /reservations/<reservation_id>

--- reserveroom_api/api_app/resources/reservations.py
+++ reserveroom_api/api_app/resources/reservations.py
@@ -105,6 +105,61 @@ class POSTReservations(Resource):
 
         return ok_response({'affected_rows': result})
 
+# PUT /reservations/<reservation_id>
+class PUTReservations(Resource):
+
+    @jwt_required
+    def put(self, reservation_id):
+        claims = get_jwt_claims()
+
+        # Get target reservation
+        sql = 'SELECT count(user_email) "count", user_email FROM reservations WHERE id=%s;'
+        try:
+            result = app.db_driver.execute_one(sql, (reservation_id))
+        except Exception as exc:
+            return error_response(404, "해당 예약 건을 찾을 수 없습니다. " + str(exc))
+        if result['count'] == 0:
+            return error_response(404, "해당 예약 건을 찾을 수 없습니다.")
+
+        # Check level
+        if claims['level'] != 9:
+            if claims['email'] != result['user_email']:
+                return error_response(403, "관리자 혹은 예약 당사자만 수정이 가능합니다.")
+
+        # Handle body parameters
+        if not request.is_json:
+            return error_response(400, 'JSON 형식으로 전달해주세요.')
+
+        try:
+            start_time = request.json.get('start_time', None)
+            end_time = request.json.get('end_time', None)
+            classroom_id = request.json.get('classroom_id', None)
+            subject = request.json.get('subject', None)
+            if not start_time:
+                return error_response(400, 'start_time을 전달해주세요.')
+            else:
+                start_time = datetime.datetime.strptime(start_time, '%Y-%m-%d %H:%M')
+            if not end_time:
+                return error_response(400, 'end_time을 전달해주세요.')
+            else:
+                end_time = datetime.datetime.strptime(end_time, '%Y-%m-%d %H:%M')
+            if not classroom_id:
+                return error_response(400, 'classroom_id를 전달해주세요.')
+            if not subject:
+                return error_response(400, 'subject를 전달해주세요.')
+        except Exception as exc:
+            return error_response(400, 'JSON 파싱 에러가 발생했습니다 : ' + str(exc))
+
+        # Querying
+        sql = 'UPDATE reservations SET start_time=%s, end_time=%s, classroom_id=%s, subject=%s WHERE id=%s;'
+        try:
+            app.db_driver.execute(sql, (start_time, end_time, classroom_id, subject, reservation_id))
+            app.db_driver.commit()
+        except Exception as exc:
+            return error_response(500, str(exc))
+        
+        return ok_response(result)
+
 # DELETE /reservations/<reservation_id>
 class DELETEReservations(Resource):
 
@@ -112,12 +167,10 @@ class DELETEReservations(Resource):
     def delete(self, reservation_id):
         claims = get_jwt_claims()
 
-        _reservation_id = int(reservation_id)
-
         # Get target reservation
         sql = 'SELECT count(user_email) "count", user_email FROM reservations WHERE id=%s;'
         try:
-            result = app.db_driver.execute_one(sql, (_reservation_id))
+            result = app.db_driver.execute_one(sql, (reservation_id))
         except Exception as exc:
             return error_response(404, "해당 예약 건을 찾을 수 없습니다. " + str(exc))
         if result['count'] == 0:
@@ -131,7 +184,7 @@ class DELETEReservations(Resource):
         # Querying
         sql = 'DELETE FROM reservations WHERE id=%s;'
         try:
-            result = app.db_driver.execute(sql, (_reservation_id))
+            result = app.db_driver.execute(sql, (reservation_id))
             app.db_driver.commit()
         except Exception as exc:
             return error_response(500, str(exc))

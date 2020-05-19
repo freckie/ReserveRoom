7863219379f1a47e2a5168815e5cbe1ddba48f50
JHyunB <whdgus4892@gmail.com>
2020-05-19 16:51:49 +0900

    Fix

--- reserveroom_api/api_app/utils.py
+++ reserveroom_api/api_app/utils.py
@@ -4,10 +4,11 @@ def is_available(reservations: list, target: tuple):
     for reservation in reservations:
         start_limit = reservation[0]-datetime.timedelta(minutes=15)
         end_limit = reservation[1]+datetime.timedelta(minutes=15)
+        print(target[0], target[1])
+        print(start_limit, end_limit)
         if (target[0] > start_limit and target[0] < end_limit) or (target[1] < end_limit and target[1] > start_limit):
-            
             return False
-        elif target[0] < start_limit and target[1] > end_limit:
+        if target[0] <= start_limit and target[1] >= end_limit:
             return False
     return True
 
